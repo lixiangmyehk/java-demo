13d73e25dc2a50f17f77158fd2fe08a5f116b3f4
lixiang <1060036793@qq.com>
2020-07-22 11:26:40 +0800

    first commit

---
+++ src/main/java/com/mye/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.mye.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String Say(){
+        return "hello world!  呵呵";
+    }
+
+}
